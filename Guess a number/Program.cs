df84829bdc35ef0699e64b9b58de7c255cd40d7a
KartofaKurti <krafter420@gmail.com>
2023-05-21 21:59:28 +0300

    dap pak probvame

--- Guess a number/Program.cs
+++ Guess a number/Program.cs
@@ -6,105 +6,117 @@ namespace Guess_a_number
     {
         static void Main(string[] args)
         {
-            /*next patch 
-            1- colors
-            2-tries counter showing to player 
-            3-final level
-            4- we will think about it
-            */
-            //variables
-            bool areTriesOut = false;
-            bool isLevelPassed = false;
-            int tries = 0;
-            int win = 0;
-            int levels = 1;
-            int maxNumber = 100;
-            int maxTries = 10;
-            //random number
-            Random randomNumber = new Random();
+            Console.ForegroundColor = ConsoleColor.Blue;
+                   
+                bool areTriesOut = false; //variables
+                int tries = 0;
+                int win = 0;
+                int levels = 1;
+                int maxNumber = 100;
+                int maxTries = 10;
+                int triesLeft = 0;  
+            
+            Random randomNumber = new Random(); //random number
             int computerRandomNumber = randomNumber.Next(1, maxNumber + 1);
             Console.WriteLine($"Welcome to level {levels}.");
-            //loop
-            while (!areTriesOut)
+            
+            while (!areTriesOut)//loop
             {              
-                //introducing to game level
-                Console.WriteLine($"Guess a number (1-{maxNumber})");
-                //player input
-                string playerGuess = Console.ReadLine();    
-                bool isValid = int.TryParse(playerGuess, out int playerNumber);
-                //game starts
-                if (isValid )
+                
+                Console.WriteLine($"Guess a number (1-{maxNumber})"); //introducing to game level
+                
+                Console.ForegroundColor = ConsoleColor.Green;//player input
+                string playerGuess = Console.ReadLine();
+                if (playerGuess == "Tries")
                 {
-                    tries++;
-                    if (playerNumber == computerRandomNumber)
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    triesLeft = maxTries - tries;
+                    Console.WriteLine($"You have {triesLeft} tries left!!!");
+                    continue;
+                }
+                bool isValid = int.TryParse(playerGuess, out int playerNumber);
+                Console.ForegroundColor = ConsoleColor.Blue;
+                
+                if (isValid) //game starts
+                {
+                    
+                    if (playerNumber == computerRandomNumber) 
                     {
-                        //win condition
-                        Console.WriteLine("You win.");
-                        win++;
-                        Console.WriteLine("Do you want to go to the next level: [y]es, [n]o");
-                        string playerInputRetry = (Console.ReadLine());
-                        if (playerInputRetry == "y")
                         {
-                                Console.WriteLine($"Welcome to level {++levels}");
-                                Console.WriteLine("Enjoy");
-                                maxNumber += 100;
+                            Console.ForegroundColor = ConsoleColor.Magenta;
+                            Console.WriteLine("You win."); //win condition
+                            win++;
+                            Console.WriteLine("Do you want to go to the next level: [y]es, [n]o");                   
+                        }
+                        string playerInputRetry = (Console.ReadLine());
+                        if (playerInputRetry == "y")  // if (playerInputRetry != "y" || playerInputRetry != "n")
+                        {
+                                Console.ForegroundColor = ConsoleColor.Magenta;
+                                Console.WriteLine($"Welcome to level {++levels}...");
+                                Console.WriteLine("Enjoy...");
+                                maxNumber += 100;                             
                                 computerRandomNumber = randomNumber.Next(1, maxNumber + 1);
                                 tries = 0;
                                 maxTries += 5;
                                 continue;
                         }
-                        //lose condition
-                        else if (playerInputRetry == "n") 
+                        else if (playerInputRetry == "n")  //exit condition
                         {
-                            Console.WriteLine("See you next time");
+                            Console.WriteLine("See you next time...");
                             break;
                         }
-                        //wrong input
-                        else
+                        else //exit condition
                         {
-                            Console.WriteLine("Invalid input");
-                        }
-                        
-                    }//hints
-                    else if (playerNumber > computerRandomNumber) 
+                            Console.WriteLine("Invalid input.");
+                        }  
+                    }
+                    else if (playerNumber > computerRandomNumber)  //hints
                     {
-                        Console.WriteLine("Too high.");                    
+                        Console.WriteLine("Too high. Go lower!!");
+                        tries++;
                     }
                     else
                     {
-                        Console.WriteLine("Too low.");                  
+                        Console.WriteLine("Too low. Go higher!!");
+                        tries++;
                     }
-                    //tries 
-                    if (tries == maxTries)
+                    if (tries == maxTries) //tries 
                     {
+                        Console.ForegroundColor = ConsoleColor.Red;
                         tries = 0;
                         Console.WriteLine("You lose..... D:");
+                        Console.WriteLine($"The number is: {computerRandomNumber} ");
                         Console.WriteLine("Do you want to play again?.... [y]es, [n]o");
                         string playerInput = (Console.ReadLine());
                         if (playerInput == "y")
                         {
-                            Console.WriteLine("Enjoy");
+                            Console.ForegroundColor = ConsoleColor.Magenta;    //Random randomRetryNumber = new Random(); int computerRetryNumber = randomRetryNumber.Next(1, maxNumber + 1);
+                            Console.WriteLine("Enjoy...");
                             continue;
-                            
                         }
                         else if (playerInput == "n")
                         {
-                            Console.WriteLine("See you next time");
+                            Console.ForegroundColor = ConsoleColor.Magenta;
+                            Console.WriteLine($"Wins: {win}");
+                            Console.WriteLine("See you next time...");
                             areTriesOut = true;
                             return;
                         }
                         else
                         {
-                            Console.WriteLine("Invalid input");
+                            Console.WriteLine("Invalid input.");
                         }
+
                     }
-                }
                 else 
                 {
-                    Console.WriteLine("Invalid input");
+                    Console.WriteLine("Invalid input.");
                 }
                 
+                }
+
             }
+
            
 
 
